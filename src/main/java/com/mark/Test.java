8fbcc0d52063fd127cba518d1e12807446ad992f
mark <mrc666888@gmail.com>
2020-04-24 16:59:28 +0800

    update

---
+++ src/main/java/com/mark/Test.java
@@ -0,0 +1,12 @@
+package com.mark;
+
+/**
+ * @author Mark
+ * @date 2020/4/24 16:59
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
