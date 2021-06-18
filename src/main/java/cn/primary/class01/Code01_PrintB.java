ec2c29c9c3d8eb04dd0039ffcbfe40dbaba08d7d
mark <mrc666888@gmail.com>
2021-06-18 16:37:58 +0800

    update

--- src/main/java/cn/primary/class01/Code01_PrintB.java
+++ src/main/java/cn/primary/class01/Code01_PrintB.java
@@ -9,7 +9,7 @@ package cn.primary.class01;
 public class Code01_PrintB {
 
     public static void main(String[] args) {
-        print(-5);
+        print(127);
         // print(~-5 + 1);
         // System.out.println(Integer.MIN_VALUE);
         // print(Integer.MIN_VALUE);
